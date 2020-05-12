4108b0a44dcc57c1859691bdf1f9cd3d37255616
John Dust <d24dust@hotmail.com>
2020-05-12 21:50:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,13 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   value_at_index = board[index]
+  
   if value_at_index == " " || value_at_index == ""
-    return false
-  elsif value_at_index == nil
     return false
   elsif value_at_index == "X" || value_at_index == "O"
     return true
+  else
+    return false
   end
     
 end
