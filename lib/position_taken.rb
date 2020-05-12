8ac3cc67ee22a42c440311d311526fb28a60548f
John Dust <d24dust@hotmail.com>
2020-05-12 21:51:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,10 +2,10 @@
 def position_taken?(board, index)
   value_at_index = board[index]
   
-  if value_at_index == " " || value_at_index == ""
-    return false
   elsif value_at_index == "X" || value_at_index == "O"
-    return true
+    return true 
+  elseif value_at_index == " " || value_at_index == ""
+    return false
   else
     return false
   end
